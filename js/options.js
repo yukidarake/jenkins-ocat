99aa3c80aa1b16419b5f307fe735b66e1d00ef66
kitakitabauer <kitahara_yuki@cyberagent.co.jp>
2013-07-10 12:51:44 +0900

    fix

--- js/options.js
+++ js/options.js
@@ -8,7 +8,7 @@ $(function() {
         $this.val(localStorage[$this.attr('name')] || '');
     });
 
-    $('#save').on('click', function() {
+    $('#save_button').on('click', function() {
         // save
         $inputs.each(function() {
             var $this = $(this);
@@ -19,5 +19,18 @@ $(function() {
 
         window.close();
     });
-});
+
+    var labels = {
+        '#options_title': 'optionstitle',
+        '#jenkins_url_label': 'jenkinsUrlLabel',
+        '#websocket_url_label': 'websocketUrlLabel',
+        '#success_noti_sec_label': 'successNotiSecLabel',
+        '#failure_noti_sec_label': 'failureNotiSecLabel',
+        '#job_regexp_label': 'jobRegexpLabel',
+        '#save_button': 'saveButton',
+    };
+
+    for (var selector in labels) {
+        $(selector).text(chrome.i18n.getMessage(labels[selector]));
+    }});
 })(jQuery);
